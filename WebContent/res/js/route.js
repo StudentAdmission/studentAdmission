94f72e7867881e6a59b6760e70de01ef0f53d08a
qq283026381 <283026381@qq.com>
2017-09-26 10:33:40 +0800

    -解决angular配合springMVC的页内路由问题

--- WebContent/res/js/route.js
+++ WebContent/res/js/route.js
@@ -1,6 +1,8 @@
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('');
     $routeProvider.when('/test', {
-        templateUrl: 'test.html'
+        templateUrl: 'test.html',
+        controller: 'testCtrl'
     })
 }]);
--- src/com/bistu/supreme/controller/IndexController.java
+++ src/com/bistu/supreme/controller/IndexController.java
@@ -2,15 +2,21 @@ package com.bistu.supreme.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 	@RequestMapping("/")
-	public String index() {
-		return "index";
+	public ModelAndView index(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		return mv;
 	}
-	@RequestMapping("/test")
-	public String test() {
-		return "test";
+
+	@RequestMapping(value ="/test")
+	public ModelAndView test(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("test");
+		return mv;
 	}
 }
