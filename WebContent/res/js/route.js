baab423fd5183e95d73ab5c9201109cd7690782d
qq283026381 <283026381@qq.com>
2017-10-09 17:32:06 +0800

    修改进入系统时的显示页面为index

--- WebContent/res/js/route.js
+++ WebContent/res/js/route.js
@@ -1,13 +1,15 @@
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('');
-    $routeProvider.when('/main', {
+    $routeProvider.when('/', {
+        controller:'indexCtrl'
+    }).when('/main', {
         templateUrl: 'main.html',
         controller: 'mainCtrl'
     }).when('/test', {
         templateUrl: 'test.html',
         controller: 'testCtrl'
     }).otherwise({
-        redirectTo: '/main'
+        redirectTo: '/'
     });
 }]);
