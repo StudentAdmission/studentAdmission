28a3d22a702f01ce43ddaa3d5589f66e29f95c64
qq283026381 <283026381@qq.com>
2017-10-09 17:31:40 +0800

    测试-angular框架以post方式传参

--- WebContent/res/js/controller/index-controller.js
+++ WebContent/res/js/controller/index-controller.js
@@ -1,12 +1,16 @@
 app.controller('indexCtrl', ['$http', '$scope', function ($http, $scope) {
     $scope.test = "123123123";
-    console.log($scope);
 }]);
 app.controller('mainCtrl', ['$http', '$scope', '$route', function ($http, $scope, $route) {
-    $scope.test="main页面";
-    console.log($scope);
+    $scope.checkEdit = function (user) {
+        console.log(user);
+        $http.post('login', user).then(function (response) {
+            console.log(response);
+        }, function (response) {
+            console.log('error');
+        })
+    }
 }]);
 app.controller('testCtrl', ['$http', '$scope', function ($http, $scope) {
     $scope.test = "test页面";
-    console.log($scope);
 }]);
