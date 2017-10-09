474dd86266b74b43a94d66290cf703aaf1041dbc
qq283026381 <283026381@qq.com>
2017-10-09 17:29:25 +0800

    不知道修改了哪里。。  代码未动，可能多了少了换行

--- src/com/bistu/supreme/controller/IndexController.java
+++ src/com/bistu/supreme/controller/IndexController.java
@@ -4,23 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class IndexController {
-	@RequestMapping(value="/index")
-	public ModelAndView index(){
+	@RequestMapping(value = "/index")
+	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("index");
 		return mv;
 	}
-	@RequestMapping(value="/main")
-	public ModelAndView main(){
+
+	@RequestMapping(value = "/main")
+	public ModelAndView main() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("main");
 		return mv;
 	}
 
-	@RequestMapping(value ="/test")
-	public ModelAndView test(){
+	@RequestMapping(value = "/test")
+	public ModelAndView test() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("test");
 		return mv;
