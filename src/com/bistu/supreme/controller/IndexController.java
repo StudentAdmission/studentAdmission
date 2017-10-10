b151d28348b0b5919f10b0e4d79a3926157d02d1
qq283026381 <283026381@qq.com>
2017-10-10 14:39:33 +0800

    对后台mapping接收前台传参进行测试

--- src/com/bistu/supreme/controller/IndexController.java
+++ src/com/bistu/supreme/controller/IndexController.java
@@ -1,9 +1,17 @@
 package com.bistu.supreme.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bistu.supreme.domain.*;
 
 @Controller
 public class IndexController {
@@ -26,5 +34,15 @@ public class IndexController {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("test");
 		return mv;
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<Map<String, String>> getUser(@RequestBody Student std) {
+		System.out.println(std);
+		System.out.println("ID" + std.getId());
+		System.out.println("name" + std.getName());
+		Map<String, String> result = new HashMap<>();
+		result.put("name", std.getName());
+		return new ResponseEntity<Map<String, String>>(result, HttpStatus.OK);
 	}
 }
