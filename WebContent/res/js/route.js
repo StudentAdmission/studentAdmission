ab8df68b91e570914e9221f9276f3ca5ee93d641
qq283026381 <283026381@qq.com>
2017-10-10 14:36:15 +0800

    无改动

--- WebContent/res/js/route.js
+++ WebContent/res/js/route.js
@@ -1,15 +1,16 @@
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $locationProvider.hashPrefix('');
-    $routeProvider.when('/', {
-        controller:'indexCtrl'
-    }).when('/main', {
-        templateUrl: 'main.html',
-        controller: 'mainCtrl'
-    }).when('/test', {
-        templateUrl: 'test.html',
-        controller: 'testCtrl'
-    }).otherwise({
-        redirectTo: '/'
-    });
-}]);
+app.config([ '$routeProvider', '$locationProvider', '$httpProvider',
+		function($routeProvider, $locationProvider, $httpProvider) {
+			$locationProvider.html5Mode(true);
+			$locationProvider.hashPrefix('');
+			$routeProvider.when('/', {
+				controller : 'indexCtrl'
+			}).when('/main', {
+				templateUrl : 'main.html',
+				controller : 'mainCtrl'
+			}).when('/test', {
+				templateUrl : 'test.html',
+				controller : 'testCtrl'
+			}).otherwise({
+				redirectTo : '/'
+			});
+		} ]);
