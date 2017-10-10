c16234e21cd51e93d4734bfbf7b3ab53aeb6c9d1
qq283026381 <283026381@qq.com>
2017-10-10 14:40:00 +0800

    对后台mapping接收前台传参进行测试，学生类

--- src/com/bistu/supreme/domain/Student.java
+++ src/com/bistu/supreme/domain/Student.java
@@ -1,18 +1,22 @@
 package com.bistu.supreme.domain;
 
 public class Student {
-private String id;
-private String name;
-public String getId() {
-	return id;
-}
-public void setId(String id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
+	private String id;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
