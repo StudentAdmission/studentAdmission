2b4f1a2dfa91f20fed8132cc8a8d57b768cdcada
qq283026381 <283026381@qq.com>
2017-10-12 09:14:22 +0800

    添加学生手册页面路由

--- WebContent/res/js/route.js
+++ WebContent/res/js/route.js
@@ -1,16 +1,20 @@
-app.config([ '$routeProvider', '$locationProvider', '$httpProvider',
-		function($routeProvider, $locationProvider, $httpProvider) {
-			$locationProvider.html5Mode(true);
-			$locationProvider.hashPrefix('');
-			$routeProvider.when('/', {
-				controller : 'indexCtrl'
-			}).when('/main', {
-				templateUrl : 'main.html',
-				controller : 'mainCtrl'
-			}).when('/test', {
-				templateUrl : 'test.html',
-				controller : 'testCtrl'
-			}).otherwise({
-				redirectTo : '/'
-			});
-		} ]);
+app.config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix('');
+        $routeProvider
+        //首页路由
+            .when('/', {
+                controller: 'indexCtrl'
+            })
+            //学生手册页面路由
+            .when('/manual', {
+                templateUrl: 'sa_manual.html',
+                controller: 'manualCtrl'
+            }).when('/test', {
+            templateUrl: 'test.html',
+            controller: 'testCtrl'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }]);
