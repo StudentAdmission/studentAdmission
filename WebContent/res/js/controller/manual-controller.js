5e477f1505eb1e7b2d9041bca1424262d9c9ba50
qq283026381 <283026381@qq.com>
2017-10-12 09:49:27 +0800

    -json数据路径修正 -获取数据后进行验证以及加载pdf文件

--- WebContent/res/js/controller/manual-controller.js
+++ WebContent/res/js/controller/manual-controller.js
@@ -1,6 +1,14 @@
 app.controller('manualCtrl', ['$http', '$scope', function ($http, $scope) {
     $scope.currentYear = 2015;
-    $http.get('data/manualData.json').then(function (response) {
-        console.log(response);
+    $http.get('res/data/manualData.json').then(function (response) {
+        console.log(response.data);
+        if (response.data.status === 200) {
+            $.each(response.data.data, function (index, item) {
+                if (item.year === $scope.currentYear) {
+                    $scope.manualUrl = item.filePath + item.fileName;
+                }
+            })
+        }
+        PDFObject.embed($scope.manualUrl, '#pdf');
     })
 }]);
