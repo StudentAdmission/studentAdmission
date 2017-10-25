ca4dd7459e8083e16ac79384a3abbf50fedfd1a3
qq283026381 <283026381@qq.com>
2017-10-25 16:28:17 +0800

    -代码格式化 -调整轮播间隔时长，从5s调整为3s

--- WebContent/res/js/controller/index-controller.js
+++ WebContent/res/js/controller/index-controller.js
@@ -1,14 +1,14 @@
 app.controller('indexCtrl', ['$http', '$scope', '$location', function ($http, $scope, $location) {
-    $scope.saLogin=function (login) {
-        $scope.login={
-            "login_num":"",
-            "login_pwd":""
+    $scope.saLogin = function (login) {
+        $scope.login = {
+            "login_num": "",
+            "login_pwd": ""
         };
-        login.login_pwd=md5(login.login_pwd);
-        $http.post('login.do',login).then(function (response) {
-            var responseData=response.data;
+        login.login_pwd = md5(login.login_pwd);
+        $http.post('login.do', login).then(function (response) {
+            var responseData = response.data;
             console.log(responseData);
-        },function (response) {
+        }, function (response) {
             console.log(response);
         })
     };
@@ -17,10 +17,15 @@ app.controller('indexCtrl', ['$http', '$scope', '$location', function ($http, $s
 
         //启动轮播图
         $("#carousel").easyFader({
-            sliderDur: 5000
+            sliderDur: 3000
         });
         $(".sa-tip").tipso({
             useTitle: false
-        })
+        });
+        $('.supreme-head-background').smint({
+            'marginTop': 0,
+            'top': 0,
+            'logo': 'show'
+        });
     });
 }]);
