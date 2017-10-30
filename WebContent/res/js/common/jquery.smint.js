766f35a7b10493690eb25266824f2e1292347182
qq283026381 <283026381@qq.com>
2017-10-30 21:59:12 +0800

    -由于系统的结构不支持使用原版smint，故对其进行修正 -clone要固定的元素，clone版元素跟随移动 -由于火狐浏览器不方便取消垂直滚动条，故将滚动条带来的左侧导航位置偏差进行修正

--- WebContent/res/js/common/jquery.smint.js
+++ WebContent/res/js/common/jquery.smint.js
@@ -3,9 +3,22 @@ $.fn.smint = function (option) {
     // get initial top offset for the menu
     var stickyTop = $(this).offset().top;
     var stickyLeft = $(this).offset().left;
-    var stickyWidth = $(this).width();
+    var clone = $(this).clone(true).css({
+        'position': 'fixed',
+        'left': stickyLeft,
+        'top': 0,
+        'width': option.width,
+        'zIndex': 9999,
+        'marginTop': option.marginTop,
+        'display': 'none'
+    });
+    $(clone).appendTo($(this).parent());
+    var ua = navigator.userAgent;
+    var firefox = "Firefox";
+    if (ua.indexOf(firefox) > -1 && option.logo !== 'show') {
+        $(clone).css('left', stickyLeft - 8.5);
+    }
     var self = this;
-
     // check position and make sticky if needed
     var stickyMenu = function () {
 
@@ -13,19 +26,12 @@ $.fn.smint = function (option) {
         var scrollTop = $(window).scrollTop();
         // if we scroll more than the navigation, change its position to fixed and add class 'fxd', otherwise change it back to absolute and remove the class
         if (scrollTop > stickyTop - option.top) {
-            $(self).css({
-                'position': 'fixed',
-                'left': stickyLeft,
-                'top': 0,
-                'width': stickyWidth,
-                'zIndex': 9999,
-                'marginTop': option.marginTop
-            });
+            $(clone).show();
             if (option.logo === 'show') {
                 $('.logo-small').fadeIn();
             }
         } else {
-            $(self).removeAttr('style');
+            $(clone).hide();
             if (option.logo === 'show') {
                 $('.logo-small').fadeOut('fast');
             }
@@ -42,6 +48,7 @@ $.fn.smint = function (option) {
 
     $(window).resize(function () {
         stickyLeft = $(self).offset().left;
+        $(clone).css('left', stickyLeft);
     })
 
 };
