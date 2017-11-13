5a91a196c07850f47026fd3b81c15f6e2d6fa7a5
qq283026381 <283026381@qq.com>
2017-11-13 15:38:35 +0800

    -将首页新闻公告和报道流程替换为从后台获取

--- WebContent/res/js/controller/index-controller.js
+++ WebContent/res/js/controller/index-controller.js
@@ -28,6 +28,49 @@ function setTime(delay) {
 
 app.controller('indexCtrl', ['$http', '$scope', 'loginService', 'introduceService', function ($http, $scope, loginService, introduceService) {
 
+    $http.post('homepageNews.do').then(function (response) {
+        var content = '';
+        if (response.status !== 200) {
+            content += '<div class="connect-error">网络异常，请检查网络连接</div>';
+        } else {
+            var homepageNews = response.data;
+            if (homepageNews.status !== 1) {
+                content += '<div class="connect-error">数据库连接失败，请联系管理员</div>';
+            } else {
+                $.each(homepageNews.data, function (index, item) {
+                    content +=
+                        '<div class="sa-list">' +
+                        '<div class="sa-item">' +
+                        '<p><a href=' + item.homepageNewsLink + '>' + item.homepageNewsTitle + '</a>' + item.homepageNewsTime + '</p>' +
+                        '</div></div>';
+                })
+            }
+        }
+        $('.sa-home-info .sa-news-content').after(content);
+    });
+
+    $http.post('homepageReportingProcess.do').then(function (response) {
+        console.log(response);
+        var content='';
+        if(response.status!==200){
+            content += '<div class="connect-error">网络异常，请检查网络连接</div>';
+        }else{
+            var homepageReportingProcess=response.data;
+            if(homepageReportingProcess.status!==1){
+                content += '<div class="connect-error">数据库连接失败，请联系管理员</div>';
+            }else{
+                $.each(homepageReportingProcess.data, function (index, item) {
+                    content +=
+                        '<div class="sa-list">' +
+                        '<div class="sa-item">' +
+                        '<p><a href=' + item.processLink + '>' + item.processItem + '</a>' + item.processTime + '</p>' +
+                        '</div></div>';
+                })
+            }
+        }
+        $('.sa-home-info .sa-reporting-content').after(content);
+    });
+
     $scope.login = {
         loginNum: "",
         loginPwd: ""
