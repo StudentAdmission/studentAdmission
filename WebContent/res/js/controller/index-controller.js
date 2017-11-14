7436d301d6ef8a169b72d52613f13ed264b39ea0
qq283026381 <283026381@qq.com>
2017-11-14 15:45:35 +0800

    -添加报道流程页面默认页面的设置

--- WebContent/res/js/controller/index-controller.js
+++ WebContent/res/js/controller/index-controller.js
@@ -26,7 +26,7 @@ function setTime(delay) {
     return date.getTime() + delay * 60000;
 }
 
-app.controller('indexCtrl', ['$http', '$scope', 'loginService', 'introduceService', function ($http, $scope, loginService, introduceService) {
+app.controller('indexCtrl', ['$http', '$scope', 'loginService', 'introduceService', 'reportingService', function ($http, $scope, loginService, introduceService, reportingService) {
 
     $scope.login = {
         loginNum: "",
@@ -70,6 +70,9 @@ app.controller('indexCtrl', ['$http', '$scope', 'loginService', 'introduceServic
     $scope.setIntroducePage = function (current) {
         introduceService.setCurrentPage(current);
     };
+    $scope.setReportingPage = function (current) {
+        reportingService.setCurrentPage(current);
+    };
     $scope.getLoginSession = function () {
         return loginService.getLoginSession() ? loginService.getLoginSession : '';
     };
@@ -93,7 +96,17 @@ app.controller('indexCtrl', ['$http', '$scope', 'loginService', 'introduceServic
             $("html,body").animate({scrollTop: 0}, 500);
         }
 
-        $('a').click(goTop);
+        $(window).on('scroll', function () {
+            var scrollTop = $(this).scrollTop();
+            if (scrollTop > 0) {
+                $('.go-top').fadeIn();
+            } else {
+                $('.go-top').fadeOut();
+            }
+        });
+        $('.go-top').on('click', function () {
+            goTop();
+        });
         $(".sa-tip").tipso({
             useTitle: false
         });
