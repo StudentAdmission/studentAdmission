74d0fef580c1b97c877a638dd7efa61f1c288f1a
2014011424 <18810912096@163.com>
2017-11-16 21:23:27 +0800

    TestNotice注释掉

--- src/com/bistu/supreme/test/TestNotice.java
+++ src/com/bistu/supreme/test/TestNotice.java
@@ -27,22 +27,22 @@ public class TestNotice {
 	@Autowired
 	INoticeDao classNoticeDao;
 
-	@Test
-	public void testGetAllNoticeOfStudent() {
-		//String studentId = "4";
-		String studentId = "201701986";
-		List<Notice> allNotice = classNoticeDao.getAllNoticeOfStudent(studentId);
-		try {
-			if (allNotice.get(0).getNoticeId() != -1) {
-				for (int i = 0; i < allNotice.size(); i++) {
-					Notice notice = allNotice.get(i);
-					System.out.println("消息名称：" + notice.getNoticeTitle() + "   发布者："
-							+ "   消息内容：" + notice.getNoticeContent() + "   消息文件中文名:" + notice.getNoticeFileEName()
-							+ "   ：消息文件英文名：" + notice.getNoticeFileCName());
-				}
-			} 
-		} catch (Exception e) {
-			System.out.println("该学生目前没有通知");
-		}
-	}
+//	@Test
+//	public void testGetAllNoticeOfStudent() {
+//		//String studentId = "4";
+//		String studentId = "201701986";
+//		List<Notice> allNotice = classNoticeDao.getAllNoticeOfStudent(studentId);
+//		try {
+//			if (allNotice.get(0).getNoticeId() != -1) {
+//				for (int i = 0; i < allNotice.size(); i++) {
+//					Notice notice = allNotice.get(i);
+//					System.out.println("消息名称：" + notice.getNoticeTitle() + "   发布者："
+//							+ "   消息内容：" + notice.getNoticeContent() + "   消息文件中文名:" + notice.getNoticeFileEName()
+//							+ "   ：消息文件英文名：" + notice.getNoticeFileCName());
+//				}
+//			} 
+//		} catch (Exception e) {
+//			System.out.println("该学生目前没有通知");
+//		}
+//	}
 }
