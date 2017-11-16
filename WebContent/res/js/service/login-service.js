1e1237916fc2e9a372b24993479c393da38079f1
qq283026381 <283026381@qq.com>
2017-11-16 11:05:43 +0800

    -添加登录时的昵称设置获取

--- WebContent/res/js/service/login-service.js
+++ WebContent/res/js/service/login-service.js
@@ -8,11 +8,17 @@ app.service('loginService', [function () {
     this.setLoginSessionPortrait = function (portrait) {
         localStorage.saPortrait = portrait;
     };
+    this.setLoginSessionNickname = function (name) {
+        localStorage.saNickname = name;
+    };
     this.getLoginSession = function () {
         return localStorage.saLogin;
     };
     this.getLoginSessionPortrait = function () {
         return localStorage.saPortrait;
+    };
+    this.getLoginSessionNickname = function () {
+        return localStorage.saNickname;
     };
     this.logout = function () {
         this.setLoginSession('');
