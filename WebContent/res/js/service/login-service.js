2b7b2382d94dc4220ed4887d6b6046b1b2a037c1
qq283026381 <283026381@qq.com>
2017-11-16 20:21:54 +0800

    -调整登录服务，值存取用户名

--- WebContent/res/js/service/login-service.js
+++ WebContent/res/js/service/login-service.js
@@ -2,27 +2,10 @@ app.service('loginService', [function () {
     this.setLoginSession = function (session) {
         localStorage.saLogin = session;
     };
-    this.setLoginSessionTag = function (tag) {
-        localStorage.saTag = tag;
-    };
-    this.setLoginSessionPortrait = function (portrait) {
-        localStorage.saPortrait = portrait;
-    };
-    this.setLoginSessionNickname = function (name) {
-        localStorage.saNickname = name;
-    };
     this.getLoginSession = function () {
         return localStorage.saLogin;
     };
-    this.getLoginSessionPortrait = function () {
-        return localStorage.saPortrait;
-    };
-    this.getLoginSessionNickname = function () {
-        return localStorage.saNickname;
-    };
     this.logout = function () {
         this.setLoginSession('');
-        this.setLoginSessionTag('');
-        this.setLoginSessionPortrait('avatar.jpg');
     }
 }]);
