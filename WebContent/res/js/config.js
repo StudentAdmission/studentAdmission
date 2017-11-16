814ff9cd00c8f3b48fc292ec7f4976c24468155c
qq283026381 <283026381@qq.com>
2017-11-16 20:22:21 +0800

    -配置文件

---
+++ WebContent/res/js/config.js
@@ -0,0 +1,8 @@
+var NETWORK_ERROR='<div class="connect-error">网络异常，请检查网络连接</div>';
+var SERVER_ERROR='<div class="connect-error">服务器连接失败，请联系管理员</div>';
+
+var ROOT_PATH='http://localhost:8080/';
+var AVATAR_PATH=ROOT_PATH+'sa-res/img/avatar/';
+var ID_PHOTO_PATH=ROOT_PATH+'sa-res/img/id-photo/';
+
+var HANDBOOK_DATA_PATH=ROOT_PATH+'sa-res/data/';
