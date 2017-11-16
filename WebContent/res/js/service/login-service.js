b66ca34260269b01f81b08e456708673832897df
qq283026381 <283026381@qq.com>
2017-11-16 09:40:59 +0800

    -添加个人头像的设置和获取方法

--- WebContent/res/js/service/login-service.js
+++ WebContent/res/js/service/login-service.js
@@ -5,11 +5,18 @@ app.service('loginService', [function () {
     this.setLoginSessionTag = function (tag) {
         localStorage.saTag = tag;
     };
+    this.setLoginSessionPortrait = function (portrait) {
+        localStorage.saPortrait = portrait;
+    };
     this.getLoginSession = function () {
         return localStorage.saLogin;
+    };
+    this.getLoginSessionPortrait = function () {
+        return localStorage.saPortrait;
     };
     this.logout = function () {
         this.setLoginSession('');
         this.setLoginSessionTag('');
+        this.setLoginSessionPortrait('avatar.jpg');
     }
 }]);
