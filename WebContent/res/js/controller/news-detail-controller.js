a9089931c542f3491f3a3f9e2623948793385881
qq283026381 <283026381@qq.com>
2017-11-17 18:03:16 +0800

    -新闻详情在页面上显示，不用append

--- WebContent/res/js/controller/news-detail-controller.js
+++ WebContent/res/js/controller/news-detail-controller.js
@@ -1,13 +1,5 @@
 app.controller('newsDetailCtrl', ['$http', '$scope', function ($http, $scope) {
     $scope.newsTitle = sessionStorage.newsTitle;
     $scope.newsTime = sessionStorage.newsTime;
-    $scope.newsContent = sessionStorage.newsContent;
-    var newsContent = $scope.newsContent.split('<br>');
-    $(function () {
-        var content = '';
-        $.each(newsContent, function (index, item) {
-            content += '<p>' + item + '</p>';
-        });
-        $('.sa-news-detail-main').find('.news-content').append(content);
-    })
+    $scope.newsContent = sessionStorage.newsContent.split('<br>');
 }]);
